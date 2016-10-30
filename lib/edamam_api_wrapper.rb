3a67dbd4d5339db6dab947add86bce5f3e1ac719
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2016-10-30 12:50:40 -0700

    base requirements plus key and id

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,12 +1,16 @@
 # require 'httparty'
 # require 'channel'
 
-class Slack_Api_Wrapper
-  TOKEN = ENV["SLACK_TOKEN"]
-  BASE_URL = "http://slack.com/api/"
+class Edamam_Api_Wrapper
+  ID = ENV["EDAMAM_ID"]
+  KEY = ENV["EDAMAM_KEY"]
+  BASE_URL = "https://api.edamam.com/"
 
-  def self.list_recipes
 
+  def self.list_recipes(search_term)
+    url = BASE_URL + "search?app_id=#{ID}" + "&app_key=#{KEY}" + "&q=#{search_term}"
+
+    response = HTTParty.get(url)
   end
 
 
