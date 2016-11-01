b587f3a323aaa950a19cb0bb9ed6324186df8ddb
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-01 12:51:29 -0700

    added show method

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,7 +1,23 @@
+require "#{Rails.root}/lib/edamam_api_wrapper.rb"
+require "#{Rails.root}/lib/recipe_list.rb"
+
 class HomepagesController < ApplicationController
   def index
+    @data = EdamamApiWrapper.list_recipes
+
+    if @data != nil && @data != []
+      render status: :created
+    else
+      render status: :service_unavailable
+    end
   end
 
   def show
+    @recipe = EdamamApiWrapper.show_recipe
+    if @recipe != nil && @recipe != []
+      render status: :created
+    else
+      render status: :service_unavailable
+    end
   end
 end
