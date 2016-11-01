d389485131499ce1afe5e577671aa683a2fcac9f
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-01 11:14:55 -0700

    created wrapper file and started to create search method.

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -2,7 +2,23 @@ require `httparty`
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search"
-  TOKEN = ENV["API_KEY"]
-  ID = ENV["API_ID"]
+  TOKEN = ENV["APP_KEY"]
+  ID = ENV["APP_ID"]
+
+  def self.list_recipes
+    url = BASE_URL + "q=#{@search_term}" + "app_id=#{ID}" + "token=#{TOKEN}"
+    data = HTTParty.get(url)
+
+    recipe_list = []
+    data["recipes"].each do |recipes|
+      label = recipe["label"]
+      image = recipe["image"]
+      url = recipe["url"]
+      recipe_list << Recipe_List.new(label, image, url)
+    end
+    return recipe_list
+  end
+
+
 
 end
