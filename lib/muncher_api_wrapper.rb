bf95e67833f90cef9ef9c5463cdb54fd95027aa6
Qlwentt <qlwentt@gmail.com>
2016-11-01 13:32:39 -0700

    write search method for muncherapiwrapper

--- lib/muncher_api_wrapper.rb
+++ lib/muncher_api_wrapper.rb
@@ -1,6 +1,15 @@
 require 'httparty'
 
 class MuncherApiWrapper
-  BASE_URL = "https://slack.com/api/"
-  TOKEN  = ENV["TOKEN"]
+  BASE_URL = "https://api.edamam.com/"
+  APP_ID  = ENV["API_ID "]
+  APP_KEY = ENV["API_KEY"]
+
+  def search(app_id=nil, app_key=nil, search_term)
+  	app_id ||= APP_ID
+  	app_key ||= APP_KEY
+  	url = BASE_URL + "search?" + "app_id=#{app_id}" + "app_key=#{app_key}" + "&pretty=1&exclude_archived=1" +"q=#{search_term}"
+  	data = HTTParty.get(url)
+  	return data
+  end
 end
