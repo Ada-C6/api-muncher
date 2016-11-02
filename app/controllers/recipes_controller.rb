48456c07f461a3446aa43e484a10c7813ece87b6
Allison Hoke <allisonhoke@gmail.com>
2016-11-02 11:48:20 -0700

    wrote index method. WIP

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,17 @@
 class RecipesController < ApplicationController
 
+  def home; end
+
   def index
+    @recipes = Recipe.search(recipe_params[:search_term])
   end
 
   def show
+    @recipe =
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:search_term)
   end
 end
