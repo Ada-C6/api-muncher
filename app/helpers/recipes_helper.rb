64fc15d819bc1ac664a73b72397788734f1315ff
Sarah Nahmias <nahmisa@gmail.com>
2016-11-01 19:43:38 -0700

    Adds new assets associated with controllers

---
+++ app/helpers/recipes_helper.rb
@@ -0,0 +1,11 @@
+module RecipesHelper
+  # def paginate_counter
+  #   begining = params["end_page"].key.to_i
+  #   increment = 10
+  #   if params["end_page"].nil?
+  #     return increment
+  #   else
+  #     return begining + increment
+  #   end
+  # end
+end
