c430ee33f9e99316c21e46fa0b934b18f70ffa54
Qlwentt <qlwentt@gmail.com>
2016-11-02 10:18:43 -0700

    revises recipe class, adding photo url and changing uri to url; starts search view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,16 @@
 class RecipesController < ApplicationController
   def index
+
   end
 
   def show
   end
 
   def search
+  	@term=params[:term]
+  	results = MuncherApiWrapper.search(@term)
+  	@my_recipes=Recipe.make_recipes_from_api(results)[0..10]
+  	
+  	#now need to format results in view
   end
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,19 +1,20 @@
 class Recipe
 
 
-  attr_reader :name, :id, :health_labels, :ingredients, :uri, :servings, :nutrients
+  attr_reader :name, :id, :health_labels, :ingredients, :url, :servings, :nutrients, :photo_url
 
   def initialize(recipe_hash)
     @name = recipe_hash[:name]
     @id = recipe_hash[:id]
     @health_labels = recipe_hash[:health_labels]
     @ingredients = recipe_hash[:ingredients]
-    @uri = recipe_hash[:uri]
+    @url = recipe_hash[:url]
     @servings = recipe_hash[:servings].to_f
     @nutrients = recipe_hash[:nutrients]
+    @photo_url = recipe_hash[:photo_url]
   end
 
-  def self.make_recipe_objs_from_api(recipe_array)
+  def self.make_recipes_from_api(recipe_array)
     recipes=[]
 
     recipe_array.each_with_index do |recipe_hash, index|
@@ -22,9 +23,10 @@ class Recipe
         id: (index+1).to_s,
         health_lables: recipe_hash['recipe']['healthLabels'],
         ingredients: Ingredient.make_ingredients_from_api(recipe_hash['recipe']['ingredients']),
-        uri: recipe_hash['recipe']['uri'],
+        url: recipe_hash['recipe']['url'],
         servings: recipe_hash['recipe']['yield'],
         nutrients: Nutrient.make_nutrients_from_api(recipe_hash['recipe']['totalNutrients'])
+        photo_url: recipe_hash['recipe']['imgage']
 
          })
     end
