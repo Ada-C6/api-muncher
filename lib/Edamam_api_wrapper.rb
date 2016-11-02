cebae07c8697d56a4a1eb95d730ae90eb7bcc528
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-01 20:30:48 -0700

    Changes to lib files to enable proper recipe index/show controller data

--- lib/Edamam_api_wrapper.rb
+++ lib/Edamam_api_wrapper.rb
@@ -1,4 +1,4 @@
-require `httparty`
+require 'httparty'
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/"
@@ -6,18 +6,27 @@ class EdamamApiWrapper
   KEY = ENV["EDAMAM_RECIPE_SEARCH_AUTH_KEY"]
 
   def self.recipe_search(search_term)
-    url = BASE_URL + "search?q=" + search_term  + "&app_id=" + ID + "&app_key=" +  KEY
+    url = BASE_URL + "search?q=" + search_term  + "&app_id=" + ID + "&app_key=" + KEY
     data = HTTParty.get(url)
     recipe_list = []
-    if !data["params"].nil?
-      data["params"].each do |hit|
-        wrapper = Recipe.new(name: hits["recipe"]["label"], recipe_image:  hits["recipe"]["image"] source_url:  hits["recipe"]["url"], ingredients: hits["recipe"]["ingredientLines"], calories: hits["recipe"]["calories"], dietary_info: hits["recipe"]["digest"], yield: hits["recipe"]["yield"], source: hits["recipe"]["source"], source_icon: hits["recipe"]["souceIcon"])
-        recipe_list.push(wrapper)
+
+    if !data["hits"].nil?
+      data["hits"].each do |hit|
+        name = hit["recipe"]["label"]
+        api_hash = {recipe_image: hit["recipe"]["image"], source_url: hit["recipe"]["url"], ingredients: hit["recipe"]["ingredientLines"], calories: hit["recipe"]["calories"].to_i,
+        dietary_info:
+        hit["recipe"]["digest"].each do |nutrient|
+          [
+            nutrient["label"], nutrient["total"].to_f, nutrient["unit"]
+          ]
+        end
+        yield: hit["recipe"]["yield"].to_i, source: hit["recipe"]["source"], source_icon: hit["recipe"]["souceIcon"]}
+        recipe = Recipe.new(name, api_hash)
+        recipe_list.push(recipe)
       end
       return recipe_list
     else
       return []
     end
   end
-
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,15 +1,16 @@
 class Recipe
-  attr_reader :name, :recipe_image, :source_url, :ingredients, :calories, :dietary_info, :yield, :source, :source_icon
+  attr_reader :name, :recipe_image, :id, :source_url, :ingredients, :calories,  :yield, :source, :source_icon #, :dietary_info
 
-  def initialize(name, options = {} )
+  def initialize(name, options = {})
     raise ArgumentError if name == nil || name == ""
 
     @name = name
-    @image = options[:image]
+    @recipe_image = options[:recipe_image]
+    @id = options[:id]
     @source_url = options[:source_url]
     @ingredients = options[:ingredients]
     @calories = options[:calories]
-    @dietary_info = options[:dietary_info]
+    # @dietary_info = options[:dietary_info]
     @yield = options[:yield]
     @source = options[:source]
     @source_icon = options[:source_icon]
@@ -19,11 +20,16 @@ class Recipe
     attr_reader :recipes
   end
 
-  def self.all
+  def self.all(search_term = nil)
     @recipes ||= EdamamApiWrapper.recipe_search(search_term)
   end
 
   def self.reset
     @recipes = nil
   end
+
+  def self.by_name(name)
+    self.all.select{|r| r.name == name }.first
+  end
+
 end
