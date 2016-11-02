65c631f055314847553f3ff10a2fd61cef07f6a6
Nicole Url <nicole.url.4@gmail.com>
2016-11-02 16:34:56 -0700

    Changed the show route a little

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get '/searches/', to: 'searches#index', as: 'searches'
   post '/searches/', to: 'searches#create', as: 'create_search'
   get '/searches/new/', to: 'searches#new', as: 'new_search'
-  get '/searches/:recipe/', to: 'searches#show', as: 'search'
+  get '/searches/:id/', to: 'searches#show', as: 'search'
 
 end
