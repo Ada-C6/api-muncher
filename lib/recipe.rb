d6cc910e165d723fdb59c801c1db64dae99315ad
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-02 10:58:17 -0700

    added end to recipe

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,9 @@
 class Recipe
-def initialize(recipe_info)
-  @name = recipe_info[:name]
-  @photo = recipe_info[:photo]
-  @url = recipe_info[:url]
-  @ingredients = recipe_info[:ingredients]
-  @dietary_labels = recipe_info[:dietary_labels]
+  def initialize(recipe_info)
+    @name = recipe_info[:name]
+    @photo = recipe_info[:photo]
+    @url = recipe_info[:url]
+    @ingredients = recipe_info[:ingredients]
+    @dietary_labels = recipe_info[:dietary_labels]
+  end
 end
