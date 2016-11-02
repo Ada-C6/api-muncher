378fbeae22003062662e51d5bfdd5253ff5a3d29
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-02 15:36:15 -0700

    url request from api is working

--- app/controllers/hompages_controller.rb
+++ app/controllers/hompages_controller.rb
@@ -1,10 +1,20 @@
+require "#{Rails.root}/lib/edemam_api_wrapper.rb"
+require "#{Rails.root}/lib/recipe.rb"
+
+
 class HompagesController < ApplicationController
+  # the results will be shown through the index page:
   def index
+
   end
 
+  # landing page where one can search for a recipe, paramaters will be brought in through a form:
   def new
+      @search = EdemamApiWrapper.request_recipes(params[:search_term])
   end
 
+  # the search results will be stored into a variable:
   def create
   end
+
 end
