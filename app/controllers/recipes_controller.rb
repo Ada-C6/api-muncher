af701bb7713effea43766e9b08bda2e589f47988
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-02 11:24:43 -0700

    add logic to remove next page if list view has less than 10

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,16 @@
 class RecipesController < ApplicationController
+  # after_action :has_recipes, only: [:next_page]
+
   def index
     @query = params["q"]
     @page = params["page"]
-    @recipe_list = EdamamApiWrapper.listrecipes(@query, @page)
+    @recipes = EdamamApiWrapper.listrecipes(@query, @page)
   end
 
   def next_page
     @page = params["data"]["page"].to_i + params["data"]["page_num"].to_i
     @query = params["data"]["query"]
-    @recipe_list = EdamamApiWrapper.listrecipes(@query, @page)
+    @recipes = EdamamApiWrapper.listrecipes(@query, @page)
   end
 
 end
