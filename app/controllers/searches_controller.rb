cda07235c5def7ef4b929c0b66ea1bffa393366e
Nicole Url <nicole.url.4@gmail.com>
2016-11-03 16:43:00 -0700

    Fixed error with url search_index number being one search behind.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -18,20 +18,7 @@ class SearchesController < ApplicationController
     render :index
   end
 
-  def next
-    params[:search_index] = params[:search_index].to_i + 10
-
-    @search_index = params[:search_index]
-    @search_word = params[:search_word]
-
-    @results ||= EdamamApiWrapper.page(@search_index, @search_word)
-
-    render :index
-  end
-
-  def previous
-    params[:search_index] = params[:search_index].to_i - 10
-
+  def page
     @search_index = params[:search_index]
     @search_word = params[:search_word]
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'searches#new', as: 'root'
   get '/searches/', to: 'searches#index', as: 'searches'
   post '/searches/', to: 'searches#create', as: 'create_search'
-  get '/searches/next/', to: 'searches#next', as: 'next'
-  get '/searches/previous/', to: 'searches#previous', as: 'previous'
+  get '/searches/page/', to: 'searches#page', as: 'page'
   get '/searches/:id/', to: 'searches#show', as: 'search'
 end
