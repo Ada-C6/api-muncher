5214e910d9287ec1a71586c6922b2163b0badcfa
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-03 16:43:05 -0700

    Add a class test

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -10,7 +10,7 @@ class EdamamApiWrapper
      url = BASE_URL + "search?q=#{ keyword }" + "&app_id=#{ TOKEN_ID }" + "&app_key=#{ TOKEN_KEYS }" + "&from=#{ (page.to_i - 1) * 10 }"
      data = HTTParty.get(url)
      recipe_list = []
-     if data["count"] > 0
+     if data["count"] != nil && data["count"] > 0
        data["hits"].each do |hit|
          wrapper = Recipe.new(hit["recipe"]["label"], hit["recipe"]["image"], hit["recipe"]["uri"], link: hit["recipe"]["url"], ingredients: hit["recipe"]["ingredients"], dietary_info: hit["recipe"]["dietLabels"])
          recipe_list << wrapper
--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -15,4 +15,16 @@ class RecipeTest < ActiveSupport::TestCase
     assert_equal r.photo, photo
     assert_equal r.uri, uri
   end
+
+  test "self.all returns a list of recipes" do
+    VCR.use_cassette("recipes") do
+      keyword = "butter"
+      page = 1
+      recipe = Recipe.all(keyword, page)
+
+      assert_kind_of Array, recipe
+      assert_kind_of Recipe, recipe.first
+    end
+  end
+  
 end
