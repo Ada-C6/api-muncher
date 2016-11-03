e88d9776d76a7e86c166e395a7ace9b212aeab36
Sarah Nahmias <nahmisa@gmail.com>
2016-11-02 21:52:02 -0700

    Makes comment more readable

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -15,7 +15,10 @@ class EdamamApiWrapper
       data["hits"].each do |recipe_hash|
         recipe = recipe_hash["recipe"]
 
-    # the last part of the URI is the recipe's unique identified. We remove the first part of the URI (RECIPE_URI_PREFIX) and use the remainder as the recipe ID.  Label is not a good candidate because it is not unique.
+    # the last part of the URI is the recipe's unique identifier.
+    # We remove the first part of the URI (RECIPE_URI_PREFIX)
+    # and use the remainder as the recipe ID.
+    # Label is not a good candidate because it is not unique.
         wrapper = Recipe.new recipe["label"], recipe["uri"].split(RECIPE_URI_PREFIX).last, image: recipe["image"], original_site: recipe["source"], original_url: recipe["url"], ingredients: recipe["ingredientLines"], diet_labels: recipe["dietLabels"], health_labels: recipe["healthLabels"]
 
         recipe_list << wrapper
