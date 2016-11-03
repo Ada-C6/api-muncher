e757be80baf55ff4685acf90ee7d20ad718c68c4
Allison Hoke <allisonhoke@gmail.com>
2016-11-03 15:22:30 -0700

    added routes for next and previous pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   # root to: "welcome#index"
   root to: "recipes#home"
+  get "recipes/next" => "recipes#next", as: "next"
+  get "recipes/previous" => "recipes#previous", as: "previous"
   resources :recipes, only: [:home, :index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
