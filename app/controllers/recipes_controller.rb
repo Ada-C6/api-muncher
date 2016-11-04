3030bc996803db87d015b3f7e8f5beb579d86d6e
Sarah Nahmias <nahmisa@gmail.com>
2016-11-04 16:00:01 -0700

    Updates due to inconsistencies identified in tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,14 +3,7 @@ class RecipesController < ApplicationController
     @keyword = params[:keyword]
     @page = params[:page].to_i
     @recipes = Recipe.all(@keyword, @page)
-
-    # save the user's recent searches.
-    # Hash prevents duplicates
-    session[:searches] ||= {}
-    session[:searches][@keyword] ||= true
-
-    @searches = session[:searches]
-
+    @searches = add_search_to_session(@keyword)
   end
 
   def show
@@ -21,8 +14,18 @@ class RecipesController < ApplicationController
     @keyword = params[:keyword]
     @page = params[:page].to_i
     @recipes = Recipe.all(@keyword, @page)
-    @searches = session[:searches]
+    @searches = add_search_to_session(@keyword)
     render :index
+  end
+
+  private
+
+  def add_search_to_session(keyword)
+    # save the user's recent searches.
+    # Hash prevents duplicates
+    session[:searches] ||= {}
+    session[:searches][keyword] ||= true
+    session[:searches]
   end
 
 end
