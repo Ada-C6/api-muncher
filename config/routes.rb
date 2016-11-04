8f282615ebc42b445e2c7ad9e219bf69d91c688e
Nicole Url <nicole.url.4@gmail.com>
2016-11-04 12:22:42 -0700

    cleaned up routes some more

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'searches#new', as: 'root'
   get '/searches/', to: 'searches#index', as: 'searches'
-  post '/searches/', to: 'searches#create', as: 'create_search'
+  post '/searches/', to: 'searches#create', as: 'create'
   get '/searches/page/', to: 'searches#page', as: 'page'
   get '/searches/:id/', to: 'searches#show', as: 'search'
 end
