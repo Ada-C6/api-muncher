685c8ac04c8ece7a66a6efe07dd38da68173da9c
Yasmin <yasminor@gmail.com>
2016-11-04 14:09:09 -0700

    Added pagination using will_paginate gem

--- app/controllers/recipe_search_controller.rb
+++ app/controllers/recipe_search_controller.rb
@@ -2,7 +2,7 @@ class RecipeSearchController < ApplicationController
 
   def index
     @query = params[:query]
-    @data = SearchResult.all(@query)
+    @data = SearchResult.all(@query).paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
