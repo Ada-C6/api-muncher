6cfe37169b8cac589a595ef48b43619df2b73f99
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-04 10:55:27 -0700

    Added site-wide footer with attribution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def year_range(start_year)
+  # In case the input was not a number (nil.to_i will return a 0)
+  start_year = start_year.to_i
+
+  current_year = Time.now.year
+
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
+  end
 end
