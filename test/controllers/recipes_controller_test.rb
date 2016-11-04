3356d20139f3fc9b3b0b9e3c398c557805d5138d
Sarah Nahmias <nahmisa@gmail.com>
2016-11-04 09:37:02 -0700

    Controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -44,4 +44,47 @@ class RecipesControllerTest < ActionController::TestCase
     end
   end
 
+  test "Next will change the recipes displayed (either previous or next)" do
+    VCR.use_cassette("recipes") do
+      # start at two so we can compare the output without dealing with corner cases.
+      # View helpers prevent navigation from occuring below page 0.
+      starting_page = 2
+      #
+      # Test for next
+      #
+      get :next, keyword: 'chicken', page: starting_page + 1
+      assert_response :success
+      assert_template :index
+
+      recipes = assigns(:recipes)
+      assert_kind_of Array, recipes
+      # check that recipes are different from those displayed on the
+      # original page
+      check_recipes = Recipe.all('chicken', starting_page)
+      recipes.each_with_index do |recipe, index|
+        assert_not_equal recipe, check_recipes[index]
+      end
+
+      assert_operator 10, :>=, recipes.length
+
+      #
+      # Test for previous
+      #
+      get :next, keyword: 'chicken', page: starting_page - 1
+      assert_response :success
+      assert_template :index
+
+      recipes = assigns(:recipes)
+      assert_kind_of Array, recipes
+      # check that recipes are different from those displayed on the
+      # original page
+      check_recipes = Recipe.all('chicken', starting_page)
+      recipes.each_with_index do |recipe, index|
+        assert_not_equal recipe, check_recipes[index]
+      end
+
+      assert_operator 10, :>=, recipes.length
+    end
+  end
+
 end
