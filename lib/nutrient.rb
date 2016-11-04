d7c0f4a3db7a9beb611ae383913b20ab8482b49e
Qlwentt <qlwentt@gmail.com>
2016-11-04 10:12:50 -0700

    fixes comma problem in csv file; attemps to fix other stuff

--- lib/nutrient.rb
+++ lib/nutrient.rb
@@ -15,6 +15,20 @@ class Nutrient
 		# end
   	end 
 
+  	def self.set_rec_values
+  		@rec_values={}
+		csv_text = File.read(File.join(Rails.root, 'app','assets','data', 'dv_data.csv'))
+		csv = CSV.parse(csv_text)
+		csv.each do |row|
+			next if row[1]==nil
+			# puts row[1]
+			# puts row[0]
+ 			@rec_values[row[0]]=row[1].to_f
+ 		end
+ 		return @rec_values
+ 	end
+
+ 	REC_VALUES=Nutrient.set_rec_values
 
 	def self.calc_percent_dv(quantity,the_rec)
 		return "no recommendation" unless the_rec.class==Float
@@ -32,23 +46,13 @@ class Nutrient
     def self.make_nutrients_from_api(api_data_hash)
 	    nutrients=[]
 
-	    @rec_values={}
-		csv_text = File.read(File.join(Rails.root, 'app','assets','data', 'dv_data.csv'))
-		csv = CSV.parse(csv_text)
-		csv.each do |row|
-			next if row[1]==nil
-			# puts row[1]
-			# puts row[0]
- 			@rec_values[row[0]]=row[1].to_f
- 		end
-
 	    api_data_hash.keys.each do |key|
 	      nutrients<<Nutrient.new({
 	        id: key,
 	        name: api_data_hash[key]['label'] ,
 	        quantity: api_data_hash[key]['quantity'].to_f,
 	        unit: api_data_hash[key]['unit'],
-	        percent_dv: calc_percent_dv(api_data_hash[key]['quantity'].to_f, @rec_values[key])
+	        percent_dv: calc_percent_dv(api_data_hash[key]['quantity'].to_f, REC_VALUES[key])
 	         })
 			
 	    end
