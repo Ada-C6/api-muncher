070276aab5cd447aace5cb34e9c27da0619307d3
Kelly Tran <trankphuong@gmail.com>
2016-11-04 14:24:24 -0700

    Added API developer account's ID and key in ENV file. Added .env fil to gitignore. Added API ID and key to API wrapper.

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -2,19 +2,25 @@ require 'httparty'
 require 'uri'
 require_relative 'recipe'
 
+require 'dotenv'
+Dotenv.load
+
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search?"
+  ID = ENV["EDAMAM_APP_ID"]
+  KEY = ENV["EDAMAM_APP_KEY"]
+  MAX_RESULTS = 100
+  # I'm making an decision to limit the search results being shown to 100
 
   def self.list_recipes(search_term)
+    url = BASE_URL + "q=#{search_term}" + "&app_id=#{ID}&app_key=#{KEY}&to=#{MAX_RESULTS}"
 
-    # I'm making an executive decision to limit the search results being shown to 100
-    url = BASE_URL + "q=#{search_term}" + "&to=100"
     data ||= HTTParty.get(url).parsed_response
 
     all_recipes = []
 
     data["hits"].each do |hit|
-      # how to parse this uri to get the unique ID string after the underscore?
+      # how to parse this uri to get the unique ID string after the underscore? use uri.fragment from the URI library.
       #http://www.edamam.com/ontologies/edamam.owl#recipe_c468dc28f8b64bb711125cc150b15c25
 
       uri = URI.parse(hit["recipe"]["uri"])
