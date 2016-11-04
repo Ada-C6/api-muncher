fe4733387a4e55e1c4ad48b4918b7d4ed6a4ff2b
LaurenSky <ljherrer@gmail.com>
2016-11-03 19:03:06 -0700

    worked on foundatoin and  added no match error message

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,13 @@ require 'will_paginate/array'
 class RecipesController < ApplicationController
   def index
     @ingredient = params[:search_term]
-    @recipes = Recipe.search(@ingredient).paginate(:page => params[:page], :per_page => 10)
+    @recipes = Recipe.search(@ingredient).paginate(:page => params[:page])
     # @page = 1
     # @recipes = Recipe.by_ten(@ingredient, @page)
   end
 
   def show
     @recipe = Recipe.by_uri(params[:uri])
+    @Page = params[:page]
   end
 end
