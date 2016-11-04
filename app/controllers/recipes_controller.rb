a41cb53ed2cc683853d23c9472e79a34f57f8eda
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-04 10:58:47 -0700

    Revisions to fix search bug; footer look

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,6 @@ class RecipesController < ApplicationController
     @page = params[:page].to_i
     Recipe.reset
     @recipes = Recipe.all(@search_term, @page)
-
   end
 
   def next
@@ -21,6 +20,8 @@ class RecipesController < ApplicationController
   # .../index?search=chocolate
 
   def show
+    # Did not realize after extracting the uri number that it could be a direct search result.
+    # (Was trying to save it as an id, then couldn't get it to map correctly.) So used this solution instead.
     @recipe = Recipe.by_name(params[:id])
   end
 end
