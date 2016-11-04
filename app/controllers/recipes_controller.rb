7f43924f34fd4a8a111a43d1c5d147c7736aa97f
Allison Hoke <allisonhoke@gmail.com>
2016-11-04 09:58:50 -0700

    added count and total pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,13 @@
 class RecipesController < ApplicationController
+  include RecipesHelper
 
   def home; end
 
   def index
     @page = (params[:page] || 1).to_i # default so you dont have to set it in the url each time
-    @recipes = EdamamApiWrapper.search(params[:search], @page)
+    @count, @recipes = EdamamApiWrapper.search(params[:search], @page)
 
-
+    @total_pages = total_pages(@count)
   end
 
   def show
