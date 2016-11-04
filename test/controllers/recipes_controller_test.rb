d7c97c4f4fff39114aec761e9285a6a4476bc74f
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-04 15:42:12 -0700

    Added controller tests. Changed index.html.erb so messages would display below buttons.

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
-class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class RecipesControllerTest < ActionController::TestCase
+
+  test "should get index" do
+    get :index, {startindex: 0}
+    assert_response :success
+    assert_template :index
+  end
+
+  test "should get show" do
+    VCR.use_cassette("recipes") do
+      get :show, {id: 'recipe_637913ec61d9da69eb451818c3293df2'}
+      assert_response :success
+      assert_template :show
+    end
+  end
 end
