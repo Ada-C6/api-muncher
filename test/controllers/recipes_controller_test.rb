bfca326df43ea1fc62105ceb220679cf59776af3
Jessica <jchung722@gmail.com>
2016-11-04 15:26:50 -0700

    added some styling and header footer

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
   #
   # test "should get show" do
   #   get :show
