edc38ab99d1dc1fef220f0b71b9ea350edde6045
LaurenSky <ljherrer@gmail.com>
2016-11-04 15:48:33 -0700

    styling got messed up

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,12 +4,14 @@ class RecipesController < ApplicationController
   def index
     @ingredient = params[:search_term]
     @recipes = Recipe.search(@ingredient).paginate(:page => params[:page], :per_page => 10)
-
+    if @recipes == []
+      flash[:empty_array] = "Sorry, there are no recipes that match your request."
+    end
   end
 
   def show
     @recipe = Recipe.by_uri(params[:uri])
-    @Page = params[:page]
+    @page = params[:page]
     @all_indgredients = @recipe.ingredientLines
     @yield = @recipe.yield.to_i
   end
