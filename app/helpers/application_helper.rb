179e7b5fe307175321fec79c73f24df7c7008b28
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-05 18:48:31 -0700

    Flash error styling for search page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def year_range(start_year)
   # In case the input was not a number (nil.to_i will return a 0)
-  start_year = start_year.to_i
+    start_year = start_year.to_i
 
-  current_year = Time.now.year
+    current_year = Time.now.year
 
     if current_year > start_year && start_year > 2000
       "#{start_year} - #{current_year}"
