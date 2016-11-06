fb4b75ea0649683e6618b478e5d27b6cf8c62cba
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-06 07:31:50 -0800

    Fixed assignment of more_items_after attribute

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
 
+  # Would have liked to have used controller filters to potentially dry up index/pagination.
+  # Got notice of being at 4000/5000 API calls for the month,
+  # so not risking it breaking things and me going over the limit trying to fix it.
+  # Would have tried to use the following controller filters:
+  
+  # before_action :set_search_term
+  # before_action :set_total_recipe_num
+  # before_action :set_more_items_after
+
   def index
     @search_term = params[:search]
     @page = params[:page].to_i
@@ -10,7 +19,13 @@ class RecipesController < ApplicationController
     end
 
     if @recipes != [] && @total_recipe_num > 100
-      @more_items_after = true
+      @recipes.each do |recipe|
+        @more_items_after = true
+      end
+    elsif @recipes != [] && @total_recipe_num <= 100
+      @recipes.each do |recipe|
+        @more_items_after = false
+      end
     end
   end
 
@@ -24,7 +39,13 @@ class RecipesController < ApplicationController
     end
 
     if @recipes != [] && @total_recipe_num > 100
-      @more_items_after = true
+      @recipes.each do |recipe|
+        @more_items_after = true
+      end
+    elsif @recipes != [] && @total_recipe_num <= 100
+      @recipes.each do |recipe|
+        @more_items_after = false
+      end
     end
 
     render :index
