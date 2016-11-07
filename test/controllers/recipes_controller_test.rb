9d321d9d4586fb72e604cbcc5d94d34a38700a53
Karin Kubischta <karin.kubischta@gmail.com>
2016-11-06 17:39:53 -0800

    test for negative show, passes

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -22,7 +22,27 @@ class RecipesControllerTest < ActionController::TestCase
   test "results should show a flash notice for a search that has no results" do
     VCR.use_cassette("recipes") do
       get :results, {search: "ththththt"}
+      assert_kind_of Array, assigns(:recipes)
+      assert_empty assigns(:recipes)
       assert_equal flash[:no_results], "Sorry! Your search term did not find any results."
+    end
+  end
+
+  test "results should display list of results when provided with a good search term" do
+    VCR.use_cassette("recipes") do
+      get :results, {search: "chicken"}
+      assert_response :success
+      assert_template :results
+      assert_kind_of Array, assigns(:recipes)
+      assert_not_empty assigns(:recipes)
+    end
+  end
+
+  test "show should show a flash notice for a request with a bad recipe identifier" do
+    VCR.use_cassette("recipes") do
+      get :show, {identifier: "chicken"}
+      assert_nil assigns(:recipe)
+      assert_equal flash[:no_results], "Sorry! we can't find this recipe."
     end
   end
 end
