f446d88b78a1ca3bfac448a4b7522518b8d7defe
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-06 18:25:39 -0800

    Finished Searches Controller tests

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,6 @@ class SearchesController < ApplicationController
   def new; end
 
   def index
-    # @todo - refactor this input check to somewhere else -- maybe edamam_api_wrapper?
     @search_term = params[:search_term].strip
 
     if @search_term.empty?
@@ -14,8 +13,6 @@ class SearchesController < ApplicationController
     @response = EdamamApiWrapper.search(@search_term)
 
     if !@response.parsed_response.nil?
-
-      # flash[:notice] = "Successfully sent search for #{@search_term}."
       @recipe_results = Recipe.search_results(@response).paginate(page: params[:page], per_page: 10)
 
       if @recipe_results.empty?
@@ -27,9 +24,6 @@ class SearchesController < ApplicationController
 
       render :new and return
     end
-
-    # render :index
-    # redirect_to recipes_path
   end
 
   def show
@@ -39,26 +33,4 @@ class SearchesController < ApplicationController
 
     @ingredients = @recipe.ingredient_objects.map { |ingredient_object| ingredient_object["text"] }
   end
-
-  def create
-    # # @todo - refactor this input check to somewhere else -- maybe edamam_api_wrapper?
-    # @search_term = params[:search_term].strip
-    #
-    # if @search_term.empty?
-    #   flash[:error] = "No search terms entered. Please enter a new search."
-    #
-    #   render :new and return
-    # end
-    #
-    # @result = EdamamApiWrapper.search(@search_term)
-    #
-    # if @result["ok"]
-    #   flash[:notice] = "Successfully sent search for #{@search_term}."
-    # else
-    #   flash[:notice] = "Failed to send search for #{@search_term}: #{@result["error"]}"
-    # end
-    # redirect_to recipes_path
-  end
-
-  # @todo in recipes controller - handle no-responses case (try searching "3453.4")
 end
--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -16,42 +16,55 @@ class SearchesControllerTest < ActionController::TestCase
   end
 
   test "3. index should display error & re-render new search if no terms entered" do
-    get :index, { search_term: "" }
+    VCR.use_cassette("search") do
+      get :index, { search_term: "" }
 
-    assert_template :new
-    assert_match /search/, flash[:error]
+      assert_template :new
+      assert_match /search/, flash[:error]
+    end
   end
 
   test "4. index should display error & re-render new search if only whitespaces entered" do
-    get :index, { search_term: "          " }
+    VCR.use_cassette("search") do
+      get :index, { search_term: "          " }
 
-    assert_template :new
-    assert_match /search/, flash[:error]
+      assert_template :new
+      assert_match /search/, flash[:error]
+    end
   end
 
-  # @todo - remove below; original version below (using create action)
-  # test "2. should create search with one search term" do
-  #   VCR.use_cassette("search") do
-  #     get :create, { search_term: "strawberry" }
-  #
-  #     assert_redirected_to recipes_path
-  #   end
-  # end
-  #
-  # test "3. should display error & re-render new search if no terms entered" do
-  #   get :create, { search_term: "" }
-  #
-  #   assert_template :new
-  #   assert_match /search/, flash[:error]
-  # end
-  #
-  # test "4. should display error & re-render new search if only whitespaces entered" do
-  #   get :create, { search_term: "          " }
-  #
-  #   assert_template :new
-  #   assert_match /search/, flash[:error]
-  # end
-  #
+  test "5. index should flash error if no results returned" do
+    VCR.use_cassette("search") do
+      get :index, { search_term: "12345" }
 
-  # @todo tests in recipes controller: - results with valid single-word input - results with valid multiple-word input -
-end
+      assert_template :index
+      assert_not_empty assigns(:response)
+      assert_empty assigns(:recipe_results)
+      assert_match /No recipes/, flash[:no_results]
+    end
+  end
+
+  test "6. show should get show page for a known uri_id" do
+    VCR.use_cassette("search") do
+      get :show, { id: "5b4049191087661cfc4219a794bdfc88" }
+
+      assert_template :show
+      assert_instance_of Recipe, assigns(:recipe)
+      assert_not_empty assigns(:ingredients)
+    end
+  end
+
+  test "7. show should get show page for a generated uri_id" do
+    VCR.use_cassette("search") do
+      original_search = EdamamApiWrapper.search("nom")
+      recipe_results = Recipe.search_results(original_search)
+      first_recipe = recipe_results.first
+
+      get :show, { id: first_recipe.uri_id }
+
+      assert_template :show
+      assert_instance_of Recipe, assigns(:recipe)
+      assert_not_empty assigns(:ingredients)
+    end
+  end
+end # class
