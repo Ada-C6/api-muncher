07ce0b374319b82e2f951f0c58e74e7aa52e2963
Shari Meggs <smeggs1@gmail.com>
2016-11-06 20:21:21 -0800

    Added two controller tests on index

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get the index page" do
+    get :index
+    assert_response :success
+  end
+
+  test "should return a collection of Recipe Objects" do
+    get :index, { name: "chicken" }
+    assert_response :success
+    assert_template :index
+  end
 end
