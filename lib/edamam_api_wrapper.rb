2aee58f39991268eeade39b388f63749e925a8fb
Yeni <yenicapote08@gmail.com>
2016-11-06 21:33:58 -0800

    made changes to recipe.rb and wrapper in order to figure out how to make the id for the show page work.

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -9,21 +9,35 @@ class EdamamApiWrapper
     url = BASE_URL + "q=#{search_term}" + "&app_id=#{ID}" + "&app_key=#{KEY}"
     data = HTTParty.get(url)
 
-    return data["hits"]
-  end
-
-  def self.list_recipes
     recipes = []
     data["hits"].each do |hit|
-      wrapper = Recipe.new(
+      recipe_hash = {
       label: hit["recipe"]["label"],
       url: hit["recipe"]["url"],
       ingredients: hit["recipe"]["ingredients"],
       diet_labels: hit["recipe"]["dietLabels"],
-      image: hit["recipe"]["image"]
-      )
+      image: hit["recipe"]["image"],
+      uri: hit["recipe"]["uri"].split("_").last
+      }
+      wrapper = Recipe.new(recipe_hash)
       recipes << wrapper
     end
     return recipes
+  end
+
+  def self.by_id(id)
+    url = BASE_URL + "r=http://www.edamam.com/ontologies/edamam.owl%23recipe_" + id + "&app_id=#{ID}" + "&app_key=#{KEY}"
+
+    recipe_info = HTTParty.get(url)
+    # recipe_hash = {
+    #   label: recipe_info[0]["label"],
+    #   url: recipe_info[0]["url"],
+    #   ingredients: recipe_info[0]["ingredients"],
+    #   diet_labels: recipe_info[0]["dietLabels"],
+    #   image: recipe_info[0]["image"],
+    #   uri: recipe_info[0]["uri"]
+    # }
+    # wrapper = Recipe.new(recipe_hash)
+    return recipe_info
   end
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,26 +1,36 @@
 class Recipe
 
-  attr_reader :label, :image, :url, :ingredients, :diet_labels
+  attr_reader :label, :image, :url, :ingredients, :diet_labels, :uri
 
-  def initialize(label, options = {})
+  def initialize(recipe_hash)
 
-    @label = label
-
-    @image = options[:image]
-    @url = options[:url]
-    @ingredients = options[:ingredients]
-    @diet_labels = options[:diet_labels]
+    @label = recipe_hash[:label]
+    @image = recipe_hash[:image]
+    @url = recipe_hash[:url]
+    @ingredients = recipe_hash[:ingredients]
+    @diet_labels = recipe_hash[:diet_labels]
+    @uri = recipe_hash[:uri]
   end
 
   class << self
     attr_reader :recipes
+    attr_reader :recipe_hash
   end
 
-  def self.all
+  def self.all(search_term)
     @recipes ||= EdamamApiWrapper.search_recipe(search_term)
+    return @recipes
   end
 
   def self.reset
-    @recipes = nil
+    #Need to figure out how to reset between searches
+  end
+
+  def self.find(id)
+    if @recipes_hash[id].nil?
+      EdamamApiWrapper.search_recipe(id)
+    else
+      return @recipes_hash[id]
+    end
   end
 end
