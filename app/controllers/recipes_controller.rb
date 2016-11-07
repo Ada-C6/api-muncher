693ff3eb4e3893fe03851f0bfbca192429d0a2ba
LaurenSky <ljherrer@gmail.com>
2016-11-06 23:10:44 -0800

    change to 12/page from 10/page on index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ require 'will_paginate/array'
 class RecipesController < ApplicationController
   def index
     @ingredient = params[:search_term]
-    @recipes = Recipe.search(@ingredient).paginate(:page => params[:page], :per_page => 10)
+    @recipes = Recipe.search(@ingredient).paginate(:page => params[:page], :per_page => 12) #changed 10 to 12 per page because of look and feel of website
     if @recipes == []
       flash[:empty_array] = "Sorry, there are no recipes that match your request."
     end
