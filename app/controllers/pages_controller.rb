e5429886a99f60ed49c85c64cb6f4fdf657a485a
KellyPT <trankphuong@gmail.com>
2016-11-07 22:59:57 -0800

    Finished Controller tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,21 @@
 class PagesController < ApplicationController
   def home
-    # This is to clear previous search_all result
-    # Recipe.reset_all
   end
 
   def index
     all_results = EdamamApiWrapper.list_recipes(params[:q])
+    # Kaminari gem to do the pagination magic
     @recipes = Kaminari.paginate_array(all_results).page(params[:page]).per(10)
+
+    # These 2 instance variables are used to store the query and the index page, so that after a user finishes exploring a particular recipe, user can still go back to where user left off. (Stop at page 5 --> venture --> go back to page 5)
     @query = params[:q]
     @page = params[:page]
-
-
-    # recipes ||= Recipe.all(params[:q])
-    # @recipes_array = Kaminari.paginate_array(recipes).page(params[:page]).per(10)
-    # Recipe.set_page(params[:page])
   end
 
   def show
-    # @recipe ||= Recipe.search_by(params[:id])
     @recipe = EdamamApiWrapper.list_a_recipe(params[:id])
+
+    # These 2 instance variables are used to store the query and the index page, so that after a user finishes exploring a particular recipe, user can still go back to where user left off. (Stop at page 5 --> venture --> go back to page 5)
     @query = params[:q]
     @page = params[:page]
   end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,19 +1,54 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  # test "should get home" do
-  #   get :home
-  #   assert_response :success
-  # end
-  #
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get :show
-  #   assert_response :success
-  # end
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_template :home
+  end
+
+  test "#index should show search results when user enters a search term" do
+    VCR.use_cassette("recipes") do
+      get :index, { q: "beef" }
+      assert_template :index
+      assert_response :success
+    end
+  end
+
+  test "#index should display error & render an empty index page if no search term was entered" do
+    VCR.use_cassette("recipes") do
+      search_term = ""
+      get :index, { q: search_term }
+      assert_template :index
+      assert_response :success
+
+      all_results = EdamamApiWrapper.list_recipes(search_term)
+      assert_kind_of Array, all_results
+      assert all_results.empty?
+      assert_equal all_results.length, 0
+    end
+  end
+
+  test "#index should display error & render an empty index page if a search query returns no result" do
+    VCR.use_cassette("recipes") do
+      search_term = "dasdsada"
+      get :index, { q: search_term }
+      assert_template :index
+      assert_response :success
+
+      all_results = EdamamApiWrapper.list_recipes(search_term)
+      assert_kind_of Array, all_results
+      assert all_results.empty?
+      assert_equal all_results.length, 0
+    end
+  end
+
+  test "#show should render a show page for a valid recipe id" do
+    VCR.use_cassette("recipes") do
+      get :show, { id: "recipe_c468dc28f8b64bb711125cc150b15c25" }
+      assert_response :success
+      assert_template :show
+    end
+  end
 
 end
