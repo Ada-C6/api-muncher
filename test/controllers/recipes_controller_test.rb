930864a4d1b3ea144f63fc52079f79b8a950d473
Jessica <jchung722@gmail.com>
2016-11-14 10:15:29 -0800

    tweek

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -5,7 +5,7 @@ class RecipesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-  #
+
   # test "should get show" do
   #   get :show
   #   assert_response :success
