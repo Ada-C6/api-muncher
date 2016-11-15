03196e9f34114656b56101cab075fea8803f2e3a
Emily Abrash <emabrash@gmail.com>
2016-11-14 22:13:06 -0800

    Fixing routes to be clearer

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,8 @@ class RecipesController < ApplicationController
   def show
     @page = 'nonhomepage'
     @recipe = Edamam_Api_Wrapper.getrecipe(params[:id])
+    @query = params[:query]
+    @from = params[:from]
 
     if !@recipe
       flash[:notice] = "Sorry, that recipe could not be found. Please try a new search!"
