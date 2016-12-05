90a0d3318374c31936c4bc55303b9e8d5ba9176b
Brandi Phillips <milligan.brandi@gmail.com>
2016-12-05 14:08:26 -0800

    reversed conditional around kaminari params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,11 +8,9 @@ class RecipesController < ApplicationController
   def index
     @search_term = params[:search_term]
     @search_results = EdemamApiWrapper.list_recipes(params[:search_term])
-    if params[:page] != nil
-      @search_results = Kaminari.paginate_array(@search_results).page(params[:page]).per(15)
-    else
-      @search_results = Kaminari.paginate_array(@search_results).page(0).per(15)
-    end
+
+    @search_results = Kaminari.paginate_array(@search_results).page(params[:page]).per(15)
+
   end
 
   def show
